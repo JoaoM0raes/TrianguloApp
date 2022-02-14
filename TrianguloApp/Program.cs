76519e4dc81ee7e7f2c186ad200ef4d29a6e593d
João Moraes <87389843+JoaoM0raes@users.noreply.github.com>
2022-02-14 15:15:47 -0300

    Update Program.cs

--- TrianguloApp/Program.cs
+++ TrianguloApp/Program.cs
@@ -61,6 +61,8 @@ namespace TrianguloApp
                 {
                     Console.Clear();
                     continue;
+                }else{
+                  break;
                 }
             }
 
